5c9f47c397b7c978350d0becbc02e45423cf03e4
Juan <juancruzg69@gmail.com>
2016-09-08 18:39:58 -0300

    No tengo idea que hay en este commit

--- src/servlet/ABMClientes.java
+++ src/servlet/ABMClientes.java
@@ -1,13 +1,20 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import entidades.Cliente;
+import entidades.Producto;
+import entidades.Venta;
 import excepciones.RespuestaServidor;
 import negocio.ControladorABM;
 import util.JsonResponses;
@@ -70,6 +77,51 @@ public class ABMClientes extends HttpServlet
 		{
 			response.setContentType("json");
 		    response.setCharacterEncoding("UTF-8");
+		    
+		    ArrayList<Cliente> listaClientes = ControladorABM.buscarTodosClientes();
+		    	
+		    JsonObject jsonResponse = new JsonObject();
+		    		    
+		    for(Cliente cliente : listaClientes)
+		    {
+			    JsonObject jsonCliente = new JsonObject();
+		    	
+		    	jsonCliente.addProperty("apellido", cliente.getApellido());
+		    	jsonCliente.addProperty("direccion", cliente.getDireccion());
+		    	jsonCliente.addProperty("id", cliente.getId());
+		    	jsonCliente.addProperty("nombre", cliente.getNombre());
+		    	jsonCliente.addProperty("telefono", cliente.getTelefono());
+		    			    	
+			    JsonArray arrayVenta = new JsonArray();
+		    	
+		    	for(Venta venta : cliente.getVentasArrayList())
+		    	{
+				    JsonObject jsonVenta = new JsonObject();
+
+				    jsonVenta.addProperty("fecha", venta.getFechaVenta().toString());
+				    jsonVenta.addProperty("formaPago", venta.getFormaPago());
+				    jsonVenta.addProperty("importe", venta.getImporte());
+		    		
+				    arrayVenta.add(jsonVenta);
+				    
+				    JsonArray arrayProducto = new JsonArray();
+				    
+		    		for(Producto producto : venta.getProductosArrayList())
+		    		{
+		    		    JsonObject jsonProducto = new JsonObject();
+
+		    		    jsonProducto.addProperty("descripcion", producto.getDescripcion());
+		    		    jsonProducto.addProperty("estado", producto.getEstado());
+		    		    jsonProducto.addProperty("id", producto.getId());
+		    		    jsonProducto.addProperty("precio", producto.getPrecio().getPrecio());
+		    		    
+					    arrayProducto.add(jsonProducto);
+		    		}
+		    	}
+		    	
+		    	jsonCliente.add("ventas", arrayVenta);
+		    }
+		    
 		    response.getWriter().write(JsonUtil.toJson(ControladorABM.buscarTodosClientes()));
 		}
 	}
